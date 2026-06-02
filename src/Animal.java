eecfae2f6a04525aa56c819ed843fd137019db52
Andrew <andrey210608@gmail.com>
2026-06-02 20:08:23 +0300

    the first entities are created

---
+++ src/Animal.java
@@ -0,0 +1,46 @@
+
+public abstract class Animal {
+
+    private final AnimalKind animalKind;
+
+    private int x;
+    private int y;
+
+    private double foodKg;
+
+    protected Animal(AnimalKind animalKind, int x, int y, double foodKg) {
+        this.animalKind = animalKind;
+        this.x = x;
+        this.y = y;
+        this.foodKg = animalKind.foodForFull;
+    }
+
+
+    public int getX() {
+        return x;
+    }
+    public int getY() {
+        return y;
+    }
+    public double getFoodKg() {
+        return foodKg;
+    }
+
+    public void setPosition(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public abstract void reproduce();
+    public abstract void move();
+    public abstract void eat();
+
+    // своя реализация
+    public void hungerTick(){
+
+    }
+
+    public boolean isDead(){
+        return foodKg <= 0;
+    }
+}
---
+++ src/Predator.java
@@ -0,0 +1,6 @@
+public abstract class Predator extends Animal {
+
+    public Predator(AnimalKind animalKind, int x, int y, double foodKg) {
+        super(animalKind, x, y, foodKg);
+    }
+}
